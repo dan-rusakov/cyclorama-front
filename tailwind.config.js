053882dba2bb99f11130911f6a9564569fe82e71
rusakov <dan-rusakov@mail.ru>
2022-06-28 04:53:32 +0500

    feat: add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,41 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
+    screens: {
+      'screen-lg': {'max': '1920px'},
+      'screen-md': {'max': '1400px'},
+      'screen-sm': {'max': '1240px'},
+      'tablet-lg': {'max': '991px'},
+      'tablet-md': {'max': '768px'},
+      'tablet-sm': {'max': '601px'},
+      'phone-lg': {'max': '480px'},
+      'phone-md': {'max': '400px'},
+      'phone-sm': {'max': '374px'},
+    },
+    colors: {
+      'white': '#ffffff',
+      'black': '#000000',
+      'black-light': '#040404',
+      'gray-light': '#adadad',
+    },
+    fontFamily: {
+      sans: ['PlayfairDisplay', 'sans-serif'],
+    },
+    fontWeight: {
+      medium: 500,
+      semibold: 600,
+      extrabold: 800,
+    },
+    container: {
+      center: true,
+      padding: '30px',
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "100%",
+        xl: "1620px"
+     }
+    },
     extend: {},
   },
   plugins: [],
